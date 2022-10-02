6263d66bd5d6a81da5a3af5c066d5baff25ae40c
askiada <25521495+askiada@users.noreply.github.com>
2022-10-02 15:39:17 +0200

    add gzip

---
+++ writer/gzip_separated_values.go
@@ -0,0 +1,46 @@
+package writer
+
+import (
+	"compress/gzip"
+	"encoding/csv"
+	"io"
+
+	"github.com/pkg/errors"
+)
+
+type GZipSeparatedValuesWriter struct {
+	w          *csv.Writer
+	gw         *gzip.Writer
+	withHeader bool
+}
+
+func NewGZipSeparatedValues(w io.Writer, separator rune) (Writer, error) {
+	gw := gzip.NewWriter(w)
+	s := &GZipSeparatedValuesWriter{
+		gw: gw,
+		w:  csv.NewWriter(gw),
+	}
+	s.w.Comma = separator
+	return s, nil
+}
+
+func (s *GZipSeparatedValuesWriter) Write(elem interface{}) error {
+	line, ok := elem.([]string)
+	if !ok {
+		return errors.Errorf("can't converte interface{} to []string: %+v", elem)
+	}
+	err := s.w.Write(line)
+	if err != nil {
+		return errors.Wrap(err, "can't write line")
+	}
+	return nil
+}
+
+func (s *GZipSeparatedValuesWriter) Close() error {
+	defer s.gw.Close()
+	s.w.Flush()
+	if s.w.Error() != nil {
+		return errors.Wrap(s.w.Error(), "can't close writer")
+	}
+	return nil
+}
